8e320b0cd310db1e8172d9a217d8d71f2334cb01
Zef Hemel <zef@zef.me>
2014-04-24 15:58:46 +0200

    Initial checkin

---
+++ check.js
@@ -0,0 +1,26 @@
+/*global _*/
+var transform = require("./transform");
+var javaScriptCheck = require("../javascript-mode/check");
+module.exports = function(info) {
+    var options = _.extend(info.options, {
+       globals: {
+           React: true
+       }
+    });
+    try {
+        var transformedCode = transform.transform(info.inputs.text);
+        return javaScriptCheck({
+            inputs: {
+                text: transformedCode
+            },
+            options: options
+        });
+    } catch(e) {
+        return [{
+            row: e.lineNumber - 1,
+            text: e.description,
+            type: "error"
+        }];
+    }
+
+};
